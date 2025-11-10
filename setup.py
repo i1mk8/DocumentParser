e028633d33f712ac38a513d72bded19cdf43a189
Aksenov Mikhail <i1mk8.ln@gmail.com>
2025-11-10 14:31:16 +0300

    Исправлена ошибка с кодировкой в setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Aksenov Mikhail',
 
     description='Пакет для разбивки различных документов (docx, pdf, изображения) на страницы, блоки, строки и слова',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
 
     url='https://github.com/i1mk8/DocumentParser',
